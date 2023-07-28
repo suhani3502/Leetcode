95d659e531a9434d915c51a9230707099d20a602
suhani3502 <87554975+suhani3502@users.noreply.github.com>
2023-07-28 08:58:04 +0530

    Time: 6 ms (75.81%), Space: 44.6 MB (19.53%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length()<k) return false;
+        
+        int count[]= new int[26];
+        
+        for (final char c : s.toCharArray()){
+            ++count[c-'a'];
+        }
+        return Arrays.stream(count).filter(c->c%2==1).count() <=k;
+    }
+}
