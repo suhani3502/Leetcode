26a9331b6854c6811b6f8f654453cec1e21ebecb
suhani3502 <87554975+suhani3502@users.noreply.github.com>
2023-08-03 01:13:29 +0530

    Time: 1 ms (77.05%), Space: 40.5 MB (84.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,36 +1,31 @@
 class Solution {
   public String decodeString(String s) {
-    Stack<Pair<StringBuilder, Integer>> stack = new Stack<>(); // (prevStr, repeatCount)
-    StringBuilder currStr = new StringBuilder();
-    int currNum = 0;
+    Stack<Integer> countStack = new Stack<>();
+        Stack<String> resultStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int count = 0;
 
-    for (final char c : s.toCharArray())
-      if (Character.isDigit(c)) {
-        currNum = currNum * 10 + (c - '0');
-      } else {
-        if (c == '[') {
-          stack.push(new Pair<>(currStr, currNum));
-          currStr = new StringBuilder();
-          currNum = 0;
-        } else if (c == ']') {
-          final Pair<StringBuilder, Integer> pair = stack.pop();
-          final StringBuilder prevStr = pair.getKey();
-          final int n = pair.getValue();
-          currStr = prevStr.append(getRepeatedStr(currStr, n));
-        } else {
-          currStr.append(c);
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                count = count * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(count);
+                resultStack.push(currentString.toString());
+                currentString = new StringBuilder();
+                count = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = new StringBuilder(resultStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
         }
-      }
 
-    return currStr.toString();
-  }
-
-  // Returns s * n.
-  private StringBuilder getRepeatedStr(StringBuilder s, int n) {
-    StringBuilder sb = new StringBuilder();
-    while (n-- > 0)
-      sb.append(s);
-    return sb;
+        return currentString.toString();
   }
 }
 
