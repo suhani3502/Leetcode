eda4f591592a3e81d66bd8e6fc48aca28ffc5155
suhani3502 <87554975+suhani3502@users.noreply.github.com>
2023-08-13 09:32:12 +0530

    Time: 1 ms (96.84%), Space: 39.7 MB (55.88%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,22 +1,14 @@
 public class Solution {
     public int addDigits(int num) {
-        while (num >= 10) {
-            // Convert the number to a string to extract individual digits
-            String numStr = String.valueOf(num);
-            
-            // Initialize the sum to 0
-            int digitSum = 0;
-            
-            // Iterate through each character (digit) in the string
-            for (char c : numStr.toCharArray()) {
-                // Convert the character back to an integer and add it to the sum
-                digitSum += Character.getNumericValue(c);
-            }
-            
-            // Update the number to the new sum
-            num = digitSum;
-        }
         
+        while(num>=10){
+            String strNum= String.valueOf(num);
+            int sum=0;
+            for(char c: strNum.toCharArray()){
+                sum=sum+ Character.getNumericValue(c);
+            }
+            num=sum;
+        }
         return num;
     }
 }
